7a89475abc23dc3b12f37329a6dd86839777678c
r-estoer3588 <entrance2sun@gmail.com>
2025-08-29 23:13:48 +0900

    refactor: reexport system core and update docs

---
+++ ui_components.py
@@ -0,0 +1,6 @@
+"""DEPRECATED shim for backward compatibility.
+Use `from common.ui_components import ...` directly.
+This module will be removed in a future release.
+"""
+
+from common.ui_components import *  # noqa: F401,F403
